7282d6d3f1693cbb9bf6bf7f5a0924d732a4bef6
Ryuga <ribereo.minami@gmail.com>
2024-01-06 23:22:41 +0900

    qiita記事を取得するAPIクライアントを作成した。

--- QiitaCombine/QiitaService.swift
+++ QiitaCombine/QiitaService.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Combine
+
+class QiitaService {
+    func fetchArticles() -> AnyPublisher<[Article], Error> {
+        guard let url = URL(string: "https://qiita.com/api/v2/tags/swift/items") else {
+            fatalError("Invalid URL")
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .print()
+            .receive(on: DispatchQueue.main)
+            .map(\.data)
+            .decode(type: [Article].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
