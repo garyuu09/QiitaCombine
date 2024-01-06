3c2f5bdd4ca09c569c0cb8443faf86ff3c603d07
Ryuga <ribereo.minami@gmail.com>
2024-01-06 17:15:40 +0900

    Initial Commit

---
+++ QiitaCombine/QiitaCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaCombineApp.swift
+//  QiitaCombine
+//
+//  Created by Ryuga on 2024/01/06.
+//
+
+import SwiftUI
+
+@main
+struct QiitaCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
